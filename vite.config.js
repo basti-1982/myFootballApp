bd16abfbef0a7d885bf38443a1cffd622d422832
dci-student <s.lonczyk@outlook.com>
2024-09-24 15:22:46 +0200

    test3

--- vite.config.js
+++ vite.config.js
@@ -14,13 +14,11 @@
 //     },
 //   },
 // })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/weatherApp/"
 })
-vite.config.js
