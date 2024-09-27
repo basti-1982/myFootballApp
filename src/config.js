2354a9c543034c8b03440af7205896f98bda10c4
dci-student <s.lonczyk@outlook.com>
2024-09-27 10:24:34 +0200

    debugging

--- src/config.js
+++ src/config.js
@@ -1,2 +1,5 @@
 export const API_KEY = "5d8948f46b674ff7a4d143618e325592";
 export const BASE_URL = "https://api.football-data.org/v4";
+
+
+// curl -X GET https://api.football-data.org/v4/competitions/2002/standings -H 'X-Auth-Token: 5d8948f46b674ff7a4d143618e325592'
