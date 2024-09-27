03f4295be191ab77b2e05cde75bd49dc7a035e89
dci-student <s.lonczyk@outlook.com>
2024-09-27 09:39:20 +0200

    debugging

--- src/components/LeagueSelector.jsx
+++ src/components/LeagueSelector.jsx
@@ -3,7 +3,7 @@ export default function LeagueSelector({ onSelect }) {
     { id: 2002, name: 'Bundesliga' }, // ID für die Bundesliga
     { id: 2021, name: 'Premier League' }, // ID für die Premier League
     { id: 2015, name: 'Ligue 1'}, // ID für die Französische Liga
-    { id: 2014, name: 'La Liga'}, // ID für die spanische Liga
+    { id: 2014, name: 'Primera Division'}, // ID für die spanische Liga
     { id: 2019, name: 'Serie A'}, // ID für die Italienische Liga
   ];
 
